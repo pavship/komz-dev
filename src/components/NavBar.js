242b7db56cd4d5780177d6ecbea55e637a6bbb9d
Pavel Shipitsyn <pavship@yandex.ru>
2018-01-28 14:44:21 +0300

    0.1.8.1 Navbar improved: + settings dropdown item. Logout now is icon

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import { NavLink } from 'react-router-dom'
 import { withRouter } from 'react-router'
 
-import { Menu, Icon, Message} from 'semantic-ui-react'
+import { Menu, Icon, Message, Dropdown} from 'semantic-ui-react'
 
 import CreateProdModal from './CreateProdModal'
 import MoveModal from './MoveModal'
@@ -22,7 +22,7 @@ class NavBar extends Component {
     return (
       // <Menu fixed='top' inverted className='addclass'>
       <div>
-        <Menu size='large' inverted >
+        <Menu size='huge' inverted >
           {/* <Container text> */}
           <Menu.Item header as={ NavLink } exact to='/'>KOMZ</Menu.Item>
           {userId &&
@@ -40,15 +40,42 @@ class NavBar extends Component {
                   <Icon name='arrow right' />
                 </Menu.Item>
               } />
+              <Dropdown item icon='options' simple>
+                <Dropdown.Menu>
+                  <Dropdown.Header>Вид</Dropdown.Header>
+                  <Dropdown.Item active>По участкам</Dropdown.Item>
+                  <Dropdown.Item>По продукции</Dropdown.Item>
+                  {/* <Dropdown.Divider />
+                  <Dropdown.Header>Участки</Dropdown.Header>
+                  <Dropdown.Item>
+                    <Icon name='square outline' />
+                    Наши
+                  </Dropdown.Item>
+                  <Dropdown.Item>
+                    <Icon name='square outline' />
+                    Подрядчики
+                  </Dropdown.Item>
+                  <Dropdown.Item>
+                    <Icon name='square outline' />
+                    Транспорт
+                  </Dropdown.Item>
+                  <Dropdown.Item>
+                    <Icon name='square outline' />
+                    Клиенты
+                  </Dropdown.Item> */}
+                </Dropdown.Menu>
+              </Dropdown>
             </Menu.Menu>
           }
           <Menu.Menu position='right'>
             {userId ?
-              <Menu.Item name='Выход' onClick={() => {
+              <Menu.Item icon onClick={() => {
                 localStorage.removeItem(GC_USER_ID)
                 localStorage.removeItem(GC_AUTH_TOKEN)
                 this.props.history.push(`/`)
-              }} />
+              }} >
+                <Icon name='sign out' />
+              </Menu.Item>
               :
               <Menu.Item name='Вход' as={ NavLink } exact to='/login' color='grey' />
             }
