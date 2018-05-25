c68a3d1e01ce3aef71a30b7a6ef28d2109501ef2
Pavel Shipitsyn <pavship@yandex.ru>
2018-05-25 13:49:09 +0300

    0.1.16 corrected prodsInProgress count

--- src/components/ModelList.js
+++ src/components/ModelList.js
@@ -24,61 +24,62 @@ class ModelList extends Component {
       const prodsReadyCount = deptModel.prodsReady.count
       const prodsDefectCount = deptModel.prodsDefect.count
       const prodsSpoiledCount = deptModel.prodsSpoiled.count
-      const prodsInProgressCount = allProdsCount - prodsReadyCount - prodsDefectCount - prodsSpoiledCount
+      const prodsInProgressCount = allProdsCount - prodsReadyCount - prodsSpoiledCount
 
       const active = _.includes(activeIndex, i)
 
       return (
-      <div key={deptModel.id} >
-        <Accordion.Title
-          active={active}
-          index={i}
-          onClick={this.handleClick}
-        >
-          <Icon name='dropdown' />
-          <Header size='small' as='span'>{deptModel.model.name}
-            <Label color='grey'>
-              {allProdsCount}
-              <Label.Detail>шт</Label.Detail>
-            </Label>
-          </Header>
-          <Label.Group className='komz-ml-21px'>
-            { (prodsReadyCount > 0) &&
-              <Label basic>
-                <Icon name='checkmark' color='green' />
-                {prodsReadyCount}
-                <Label.Detail>ГП</Label.Detail>
+        <div key={deptModel.id} >
+          <Accordion.Title
+            active={active}
+            index={i}
+            onClick={this.handleClick}
+          >
+            <Icon name='dropdown' />
+            <Header size='small' as='span'>{deptModel.model.name}
+              <Label color='grey'>
+                {allProdsCount}
+                <Label.Detail>шт</Label.Detail>
               </Label>
-            }
-            { (prodsInProgressCount > 0) &&
-              <Label basic>
-                {prodsInProgressCount}
-                <Label.Detail>НЗП</Label.Detail>
-              </Label>
-            }
-            { (prodsDefectCount > 0) &&
-              <Label basic>
-                <Icon name='warning sign' color='orange' />
-                {prodsDefectCount}
-                <Label.Detail>ОТКЛОН</Label.Detail>
-              </Label>
-            }
-            { (prodsSpoiledCount > 0) &&
-              <Label basic>
-                <Icon name='broken chain' color='red' />
-                {prodsSpoiledCount}
-                <Label.Detail>БРАК</Label.Detail>
-              </Label>
-            }
-          </Label.Group>
-        </Accordion.Title>
-        { active &&
-          <Accordion.Content active>
-            <ProdList prods={deptModel.prods} selectProd={this.props.selectProd}/>
-          </Accordion.Content>
-        }
-      </div>
-    )})
+            </Header>
+            <Label.Group className='komz-ml-21px'>
+              {(prodsReadyCount > 0) &&
+                <Label basic>
+                  <Icon name='checkmark' color='green' />
+                  {prodsReadyCount}
+                  <Label.Detail>ГП</Label.Detail>
+                </Label>
+              }
+              {(prodsInProgressCount > 0) &&
+                <Label basic>
+                  {prodsInProgressCount}
+                  <Label.Detail>НЗП</Label.Detail>
+                </Label>
+              }
+              {(prodsDefectCount > 0) &&
+                <Label basic>
+                  <Icon name='warning sign' color='orange' />
+                  {prodsDefectCount}
+                  <Label.Detail>ОТКЛОН</Label.Detail>
+                </Label>
+              }
+              {(prodsSpoiledCount > 0) &&
+                <Label basic>
+                  <Icon name='broken chain' color='red' />
+                  {prodsSpoiledCount}
+                  <Label.Detail>БРАК</Label.Detail>
+                </Label>
+              }
+            </Label.Group>
+          </Accordion.Title>
+          {active &&
+            <Accordion.Content active>
+              <ProdList prods={deptModel.prods} selectProd={this.props.selectProd} />
+            </Accordion.Content>
+          }
+        </div>
+      )
+    })
 
     return (
       <Accordion>
